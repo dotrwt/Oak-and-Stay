e1860e1408741c639a4a576958d427a553df13c3
Harshvardhan Rawat <fest.nov01@gmail.com>
2026-01-24 10:51:57 +0530

    edit upload files

--- controller/listings.js
+++ controller/listings.js
@@ -22,8 +22,13 @@ module.exports.showListing = async (req, res) => {
 };
 
 module.exports.createListing = async (req, res) => {
+    let url = req.file.path;
+    let filename = req.file.filename;
+
     const newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
+    newListing.image = {url, filename};
+    
     await newListing.save();
     req.flash('success', 'Successfully created a new listing!');
     res.redirect("/listings");
@@ -44,6 +49,14 @@ module.exports.renderEditForm = async (req, res) => {
 module.exports.updateListing = async (req, res) => {
     let {id} = req.params;
     let listing = await Listing.findByIdAndUpdate(id, {...req.body.listing});
+
+    if(typeof req.file !== "undefined") {
+        let url = req.file.path;
+        let filename = req.file.filename;
+        listing.image = {url, filename};
+        await listing.save();
+    };
+
     req.flash('success', 'Successfully updated the listing!');
     res.redirect(`/listings/${listing._id}`);
 };
--- routes/listing.js
+++ routes/listing.js
@@ -4,6 +4,9 @@ const wrapAsync = require('../utils/wrapAsync.js');
 const { isLoggedIn, isOwner, validateListing} = require('../middleware.js');
 const methodOverride = require('method-override');
 const listingController = require('../controller/listings.js');
+const multer  = require('multer')
+const { storage } = require('../cloudConfig.js');
+const upload = multer({storage});
 
 
 //Method Override setup
@@ -15,7 +18,7 @@ router.use(expres.urlencoded({ extended: true }));
 //Index and Create route
 router.route('/')
 .get (wrapAsync(listingController.index))
-.post(validateListing, isLoggedIn, wrapAsync(listingController.createListing));
+.post(validateListing, isLoggedIn, upload.single("listing[image]") ,wrapAsync(listingController.createListing));
 
 //New route
 router.get('/new', isLoggedIn, listingController.renderNewForm);
@@ -23,7 +26,7 @@ router.get('/new', isLoggedIn, listingController.renderNewForm);
 //Show, Update and Delete route
 router.route('/:id')
 .get(wrapAsync(listingController.showListing))
-.put(isLoggedIn, isOwner,wrapAsync(listingController.updateListing))
+.put(isLoggedIn, isOwner,validateListing, upload.single("listing[image]") ,wrapAsync(listingController.updateListing))
 .delete(isLoggedIn, isOwner, wrapAsync(listingController.deleteListing));
 
 
